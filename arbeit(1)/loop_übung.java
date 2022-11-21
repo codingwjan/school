b88879bc6a64171dac930d500f053c624f4f7c80
jan pink <107748760+ebayboy123@users.noreply.github.com>
2022-11-21 18:25:14 +0100

    added questions

---
+++ arbeit(1)/loop_übung.java
@@ -0,0 +1,12 @@
+//import scanner
+import java.util.Scanner;
+
+public class main {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		boolean b = true; 
+		while(b == true) {
+			System.out.println("loop is active");
+		}
+	}
+}
