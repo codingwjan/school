227e8c9d2c47c305c153d3baede6f34eaf741eed
jan pink <02-schablone.depots@icloud.com>
2022-11-29 10:45:31 +0100

    fix: primzahl

--- Berichtigung/jan/Aufgabe2.java
+++ Berichtigung/jan/Aufgabe2.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
 public class Aufgabe2 {
-    main method
+    public static void main(String args[]) {
+        //eingabezahl
+        int eingabeZahl;
+        int aktuellerDurchlauf = 2;
+        int durchlaeufe = 2;
+        int teilbarkeitZaeler = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bitte geben Sie eine Zahl ein: ");
+        eingabeZahl = input.nextInt();
+
+        while (eingabeZahl/2 >= durchlaeufe) {
+            
+            durchlaeufe++;
+            if (eingabeZahl % durchlaeufe == 0) {
+                teilbarkeitZaeler++;
+            }
+        }
+        if (teilbarkeitZaeler == 0) {
+            System.out.println("Die Zahl ist eine Primzahl");
+        } else {
+            System.out.println("Die Zahl ist keine Primzahl");
+        }
+    }
 }
