3c92f9bfc44b48cc60497ffefb0c906b74c88de2
jan pink <02-schablone.depots@icloud.com>
2022-11-29 11:00:27 +0100

    fix: num 4 not prime

--- Berichtigung/jan/Aufgabe2.java
+++ Berichtigung/jan/Aufgabe2.java
@@ -3,14 +3,17 @@ public class Aufgabe2 {
     public static void main(String args[]) {
         //eingabezahl
         int eingabeZahl;
-        int aktuellerDurchlauf = 2;
         int durchlaeufe = 2;
         int teilbarkeitZaeler = 0;
         Scanner input = new Scanner(System.in);
         System.out.println("Bitte geben Sie eine Zahl ein: ");
         eingabeZahl = input.nextInt();
+        if (eingabeZahl == 4) {
+            System.out.println("Die Zahl ist keine Primzahl");
+            System.exit(0);
+        }
 
-        while (eingabeZahl/2 >= durchlaeufe) {
+        while (eingabeZahl/2 > durchlaeufe) {
             
             durchlaeufe++;
             if (eingabeZahl % durchlaeufe == 0) {
