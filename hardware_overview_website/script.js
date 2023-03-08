face7660edea71d70ab9e4230480e8479cc899ec
jan pink <02-schablone.depots@icloud.com>
2023-03-08 08:59:35 +0100

    add: feature search bar

--- hardware_overview_website/script.js
+++ hardware_overview_website/script.js
@@ -195,3 +195,17 @@ function openWiki(input) {
     window.open(input, '_blank');
 
 }
+
+function search() {
+    let input = document.getElementById('search').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('hardwareComponent');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        } else {
+            x[i].style.display = "flex";
+        }
+    }
+}
