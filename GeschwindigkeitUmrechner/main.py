07f15826c74e3b61ccae98873e193c22c80888bf
jan pink <02-schablone.depots@icloud.com>
2023-03-17 09:53:38 +0100

    chore: function

--- GeschwindigkeitUmrechner/main.py
+++ GeschwindigkeitUmrechner/main.py
@@ -1,16 +1,26 @@
-sourceSize = input("Please enter how large the USB is in GB: ")
-sourceSize = int(sourceSize)
-sourceSpeed = input("Please enter the speed of the USB in MB/s: ")
-sourceSpeed = int(sourceSpeed)
+def conversion(sourceSize, sourceSpeed):
 
-#calculate the time it takes to copy the files
-time = (sourceSize * 1000) / sourceSpeed
-time = round(time, 2)
-
-#if time is less than 60 seconds, print seconds
-if time > 60:
-    time = time / 60
+    #calculate the time it takes to copy the files
+    time = (sourceSize * 1000) / sourceSpeed
     time = round(time, 2)
-    print("It will take " + str(time) + " minutes to copy the files")
-else:
-    print("It will take " + str(time) + " seconds to copy the files")
+
+    #if time is less than 60 seconds, print seconds
+    if time > 60:
+        time = time / 60
+        time = round(time, 2)
+        print("It will take " + str(time) + " minutes to copy the files")
+    else:
+        print("It will take " + str(time) + " seconds to copy the files")
+
+
+def main():
+    sourceSize = input("Please enter how large the USB is in GB: ")
+    sourceSpeed = input("Please enter the speed of the USB in MB/s: ")
+    sourceSpeed = int(sourceSpeed)
+    sourceSize = int(sourceSize)
+
+    conversion(sourceSize, sourceSpeed)
+
+
+if __name__ == "__main__":
+    main()
