aa22f11e5ff953662035c1ea18edf064fd795a00
cancel-cloud <Laggis_131@icloud.com>
2023-06-20 21:59:28 +0200

    🧪 fix: client & teacher view

--- kahoot_tkdv/src/views/clientuserlist.jsx
+++ kahoot_tkdv/src/views/clientuserlist.jsx
@@ -4,14 +4,15 @@ export default function Clientuserlist() {
     //get the list of users from the server
     const [people, setPeople] = useState([]);
     useEffect(() => {
-        fetch("http://0.0.0.0:8420/game/info?gameId="+localStorage.getItem("gameId"))
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                setPeople(data);
-            })
-    }
-    , [])
+            fetch("http://0.0.0.0:8420/game/info?gameId="+localStorage.getItem("gameId"))
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    console.log(data.students[0].profilePicture)
+                    setPeople(data.students || []);
+                })
+        }
+        , [])
     return (
         <div className={"bg-white p-10"}>
             <div className={"flex flex-col"}>
@@ -23,12 +24,11 @@ export default function Clientuserlist() {
             </div>
             <ul role="list" className="divide-y divide-gray-100 mt-5">
                 {people.map((person) => (
-                    <li key={person.email} className="flex justify-between gap-x-6 py-5">
+                    <li key={person.name} className="flex justify-between gap-x-6 py-5">
                         <div className="flex gap-x-4">
-                            <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt=""/>
-                            <div className="min-w-0 flex-auto">
-                                <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
-                                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
+                            <img className="h-12 w-12 object-cover flex-none rounded-full bg-gray-50" src={person.profilePicture} alt=""/>
+                            <div className="min-w-0 flex-auto h-full flex items-center">
+                                <p className="text-2xl font-bold leading-6 text-gray-900">{person.name}</p>
                             </div>
                         </div>
                     </li>
--- kahoot_tkdv/src/views/userlist.jsx
+++ kahoot_tkdv/src/views/userlist.jsx
@@ -6,7 +6,12 @@ export default function Userlist() {
         fetch("http://0.0.0.0:8420/game/info?gameId="+localStorage.getItem("gameId"))
             .then(res => res.json())
             .then(data => {
-                setPeople(data);
+                if (Array.isArray(data.students)) {
+                    setPeople(data.students); // only take the students array from the data
+                } else {
+                    // handle the case where data.students is not an array
+                    setPeople([]); // set to an empty array, or handle this situation as you see fit
+                }
             })
     }, [])
 
@@ -44,10 +49,11 @@ const UserCard = ({ person, postition }) => {
         <li key={person.email} className="flex justify-between gap-x-6 py-5">
             <div className="flex gap-x-4">
                 <div className={"text-3xl font-medium flex items-center"}>{postition}</div>
-                <img className="h-20 w-20 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt=""/>
-                <div className="min-w-0 h-full my-auto">
-                    <p className="text-3xl font-semibold leading-6 text-gray-900">{person.name}</p>
-                    <p className="mt-3 truncate text-2xl font-bold leading-5 text-gray-500">{person.score}</p>
+                <img className="h-20 w-20 flex-none rounded-full bg-gray-50" src={person.profilePicture} alt=""/>
+                <div className="flex gap-x-4">
+                    <div className="min-w-0 flex-auto h-full flex items-center">
+                        <p className="text-4xl font-bold leading-6 text-gray-900">{person.name}</p>
+                    </div>
                 </div>
             </div>
         </li>
